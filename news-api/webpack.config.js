20580f6c557818c103c9263c2c4dd5136f999577
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-02 02:19:14 +0300

    chore: edit webpack and eslint config

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -1,28 +1,40 @@
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: './src/index',
     mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader']
             },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader'
+            }
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js']
     },
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
     },
     plugins: [
+        new ESLintPlugin({
+            extensions: ['ts'],
+            eslintPath: require.resolve('eslint/use-at-your-own-risk'),
+            failOnError: false,
+            emitWarning: true
+        }),
         new DotenvWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
@@ -34,7 +46,9 @@ const baseConfig = {
 
 module.exports = ({ mode }) => {
     const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+    const envConfig = isProductionMode
+        ? require('./webpack.prod.config')
+        : require('./webpack.dev.config');
 
     return merge(baseConfig, envConfig);
 };
