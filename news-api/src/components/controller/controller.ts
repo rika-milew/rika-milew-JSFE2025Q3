f246222ee6a457dc1c6fb9e97f40bfaa386da140
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-02 18:33:27 +0300

    feat: migrate controlller.js to ts

---
+++ news-api/src/components/controller/controller.ts
@@ -0,0 +1,47 @@
+import AppLoader from './appLoader';
+
+class AppController extends AppLoader {
+  getSources(callback: (data: unknown) => void): void {
+    super.getResp(
+      {
+        endpoint: 'mocks/sources',
+        options: {},
+      },
+      callback
+    );
+  }
+
+  getNews(e: Event, callback: (data: unknown) => void): void {
+    let target: EventTarget | null = e.target;
+    if (!(target instanceof HTMLElement)) return;
+
+    const newsContainer = e.currentTarget;
+    if (!(newsContainer instanceof HTMLElement)) return;
+
+    while (target && target !== newsContainer) {
+      if (target instanceof HTMLElement && target.classList.contains('source__item')) {
+        const sourceId = target.getAttribute('data-source-id');
+        if (sourceId && newsContainer.getAttribute('data-source') !== sourceId) {
+          newsContainer.setAttribute('data-source', sourceId);
+          super.getResp(
+            {
+              endpoint: 'mocks/everything',
+              options: {
+                sources: sourceId,
+              },
+            },
+            callback
+          );
+        }
+        return;
+      }
+      if (target instanceof HTMLElement) {
+        target = target.parentElement;
+      } else {
+        return;
+      }
+    }
+  }
+}
+
+export default AppController;
