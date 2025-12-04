bd07b0ad38b83924116835d71a2d1529b84e9b87
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-04 03:17:40 +0300

    feat: edit prod config

--- news-api/webpack.prod.config.cjs
+++ news-api/webpack.prod.config.cjs
@@ -2,12 +2,13 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-
     output: {
-        publicPath: './', 
+        publicPath: '/news-api/', 
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
     },
-
+    optimization: {
+        minimize: false, 
+    },
     devtool: 'source-map',
 };
