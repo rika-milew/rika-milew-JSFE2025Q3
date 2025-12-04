7ccab1cefa2da8b9b4ca23657fd4ceef0a48d50f
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-05 00:03:23 +0300

    feat: edit apikey in apploader

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     const apiUrl: string = process.env.API_URL ?? '';
-    const apiKey: string = process.env.API_KEY ?? '';
+    const apiKey: string = process.env.API_KEY ? String(process.env.API_KEY) : '';
     super(apiUrl, { apiKey });
   }
 }
