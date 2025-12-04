215721dc3b7218ac7a57071252ae741cacbb63f3
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-04 03:31:18 +0300

    fix: images and favicon path

--- news-api/webpack.prod.config.cjs
+++ news-api/webpack.prod.config.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     output: {
-        publicPath: '/news-api/', 
+        publicPath: './', 
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
     },
