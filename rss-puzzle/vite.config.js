1e67e343d17ae59d36c61b5db03876395f6564d8
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-15 14:12:00 +0300

    chore: RSS-PZ-00 Project setup and Vite configuration

--- rss-puzzle/vite.config.js
+++ rss-puzzle/vite.config.js
@@ -1,28 +1,23 @@
 import Inspect from 'vite-plugin-inspect';
 import { resolve } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import checker from 'vite-plugin-checker';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default {
   plugins: [
     Inspect(),
-    ViteImageOptimizer({
-      png: {
-        quality: 90,
-      },
-      jpg: {
-        quality: 80,
-      },
-      webp: {
-        quality: 80,
-      },
-    }),
+    // ViteImageOptimizer({
+    //   png: {
+    //     quality: 90,
+    //   },
+    //   jpg: {
+    //     quality: 80,
+    //   },
+    //   webp: {
+    //     quality: 80,
+    //   },
+    // }),
     tsconfigPaths(),
-    checker({
-      typescript: true,
-      eslint: { lintCommand: 'eslint "./src/**/*.{ts,js}"' }
-    }),
   ],
   base: './',
   build: {
