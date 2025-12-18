ef3e0d2974300fc80eecbd91270c6df0fb674597
Erika Milevskaya <95300271+rika-milew@users.noreply.github.com>
2025-12-18 21:25:21 +0300

    feat: RSS-PZ-05 Implement Start Screen Design (#67)

--- rss-puzzle/.stylelintrc.cjs
+++ rss-puzzle/.stylelintrc.cjs
@@ -28,7 +28,7 @@ module.exports = {
         { properties: ['border', 'border-width', 'border-style', 'border-color', 'border-radius', 'box-shadow'] },
         { properties: ['list-style-type', 'overflow', 'overflow-x', 'overflow-y'] },
         { properties: ['font', 'font-size', 'font-family', 'font-weight', 'line-height', 'color', 'text-align'] },
-        { properties: ['background', 'background-color', 'outline', 'text-decoration'] },
+        { properties: ['background', 'background-color', 'background-image', 'outline', 'text-decoration'] },
         { properties: ['cursor', 'transition', 'transform', 'opacity', 'animation'] },
       ],
       {
