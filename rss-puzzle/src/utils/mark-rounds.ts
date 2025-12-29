4d0a0d9d9f2433c237620fc4a526ac02c13a8f50
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-30 00:27:50 +0300

    feat: RSS-PZ-26 add indicators for completed levels and rounds

---
+++ rss-puzzle/src/utils/mark-rounds.ts
@@ -0,0 +1,22 @@
+import { eventState } from '../pages/game/event-state';
+import { gameState } from '../pages/game/game-state';
+import { levelRounds } from '../pages/game/levels/level-storage';
+import { progressState } from '../pages/game/levels/progress-state';
+
+export function markRounds(): void {
+  const { levelIndex, roundIndex } = gameState;
+
+  if (!progressState.completedRounds.has(levelIndex)) {
+    progressState.completedRounds.set(levelIndex, new Set());
+  }
+
+  const roundsSet = progressState.completedRounds.get(levelIndex) ?? new Set<number>();
+  roundsSet.add(roundIndex);
+  progressState.completedRounds.set(levelIndex, roundsSet);
+
+  if (roundsSet.size === levelRounds[levelIndex]) {
+    progressState.completedLevels.add(levelIndex);
+  }
+
+  eventState.emit('progress:updated', true);
+}
