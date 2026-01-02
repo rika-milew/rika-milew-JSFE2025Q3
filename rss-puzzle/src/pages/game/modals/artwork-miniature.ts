f1ab807dd218ff6a540c7290667499ced17575af
Erika Milevskaya <95300271+rika-milew@users.noreply.github.com>
2026-01-03 00:33:06 +0300

    feat: RSS-PZ-33 Display Artwork On Statistics Page (#93)
    * feat: RSS-PZ-33 add cut images
    
    * feat: RSS-PZ-33 display artwork miniature on statistics page

---
+++ rss-puzzle/src/pages/game/modals/artwork-miniature.ts
@@ -0,0 +1,42 @@
+import { createElement } from '../../../utils/create-element';
+import { gameState } from '../game-state';
+
+export function displayMiniature(): HTMLElement {
+  const container = createElement({
+    tag: 'div',
+    className: 'miniature',
+  });
+
+  const artwork = createElement({
+    tag: 'img',
+    className: 'miniature__image',
+    attributes: {
+      src: `/pictures/${gameState.levelImage}`,
+      alt: gameState.imageName,
+    },
+  });
+
+  artwork.style.backgroundImage = `url(/pictures/${gameState.cutImage})`;
+
+  const title = createElement({
+    tag: 'div',
+    className: 'miniature__title',
+    textContent: gameState.imageName,
+  });
+
+  const author = createElement({
+    tag: 'div',
+    className: 'miniature__author',
+    textContent: gameState.author,
+  });
+
+  const year = createElement({
+    tag: 'div',
+    className: 'miniature__year',
+    textContent: gameState.year,
+  });
+
+  container.append(artwork, title, author, year);
+
+  return container;
+}
--- rss-puzzle/src/pages/game/modals/results-modal.ts
+++ rss-puzzle/src/pages/game/modals/results-modal.ts
@@ -1,6 +1,8 @@
 import { createModal } from '../../../components/modal/modal';
 import { createElement } from '../../../utils/create-element';
 import { eventState } from '../event-state';
+import { displayMiniature } from './artwork-miniature';
+import { gameState } from '../game-state';
 
 import './results-modal.css';
 
@@ -15,14 +17,21 @@ export function openResultsModal(
 
   const title = createElement({
     tag: 'h2',
+    className: 'results__title',
     textContent: 'Round Statistics',
   });
 
   content.append(title);
 
+  if (gameState.cutImage) {
+    const artworkMiniature = displayMiniature();
+    content.append(artworkMiniature);
+  }
+
   createModal({
     container,
     content,
+    modalClassName: 'results-modal',
     buttons: [
       {
         text: 'Continue',
